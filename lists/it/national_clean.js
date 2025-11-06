2a23a301bd944e5d6741e1470b0edc308ebe660f
johngrischam <57707830+johngrischam@users.noreply.github.com>
2025-11-06 03:24:21 +0100

    Update national_clean.js

--- lists/it/national_clean.js
+++ lists/it/national_clean.js
@@ -16,7 +16,7 @@ export default {
     fetch: fetchBlueEPG,
     channels: [
       2064, 2015, 1948, 1665, 1386,
-      1379, 1346, 1287, 1123, 613
+      1379, 1346, 1287, 613, 393, 357
     ],
   },
 
@@ -24,8 +24,8 @@ export default {
   blue_b: {
     fetch: fetchBlueEPG,
     channels: [
-      393, 357, 356, 348, 346,
-      338, 334, 332, 329, 328
+      356, 348, 346, 338, 334, 
+      332, 329, 328, 327, 266 
     ],
   },
 
@@ -33,7 +33,7 @@ export default {
   blue_c: {
     fetch: fetchBlueEPG,
     channels: [
-      327, 266, 257, 243, 239,
+      257, 243, 239,
       237, 216, 215, 214, 191,
       118, 96, 79, 51
     ],
@@ -45,4 +45,9 @@ export default {
     channels: ["*"], // expands automatically from XML
   },
 
+  // --- SuperGuidaTV: Sky / Pay-TV channels only ---
+  superguidatv: {
+    fetch: fetchSuperGuidaEPG,
+    channels: [1], // Sky Uno
+  },
 }; 
